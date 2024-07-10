bbf0233ea20e4abf182da240a53386693bcd9d83
opu mia <opumia7799@gmail.com>
2024-07-10 06:51:11 +0000

    datatype

---
+++ myJS/variable.js
@@ -0,0 +1,12 @@
+const accountId = 778899
+let accountEmail = ("opumia7799@gmail.com")
+var accountPassword = "123456"
+let accountCity = "sylhet"
+
+console.table([ accountId, accountEmail,accountCity])
+
+ accountEmail = ("opu@77gmail.com")
+ let accounname = "opu"
+ let accountstate;
+  
+ console.table([accountEmail, accounname, accountstate])
