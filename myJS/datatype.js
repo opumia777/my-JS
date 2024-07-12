82564e3befd09975a970f2b77f12906fd85ca764
opu mia <opumia7799@gmail.com>
2024-07-12 05:36:44 +0000

    datatype

---
+++ myJS/datatype.js
@@ -0,0 +1,45 @@
+
+const myname = ("opu mia")
+console.log(myname);
+
+//-----operation----
+let value = 7
+let negvalue = -value
+// console.log( negvalue);
+
+let numberis =10
+let negnumberis = -numberis
+
+//console.log( negnumberis);
+
+ let str1 = "Hello"
+ let str2 = " HI"
+ let str3 = " i am opu"
+
+ let all = str1+str2+str3
+
+ //console.log(all);
+
+ console.log(2 + 2);
+// Expected output: 4
+
+console.log(2 + true);
+// Expected output: 3
+
+console.log('hello ' + 'everyone');
+// Expected output: "hello everyone"
+
+console.log(2001 + ': A Space Odyssey');
+// Expected output: "2001: A Space Odyssey"
+
+//primitive......
+
+//7types: string , number ,boolearn, null,undefined,symbols, binint
+
+//Reference  ( non primitive)....
+
+//3 type: Array, objects, function 
+
+const myfunction = function(){
+    console.log( "Hello World ");
+}
