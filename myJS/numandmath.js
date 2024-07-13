6bb672844062643e6172b0766102e54ee367234b
opu mia <opumia7799@gmail.com>
2024-07-13 01:44:24 +0000

    numberandmath

---
+++ myJS/numandmath.js
@@ -0,0 +1,2 @@
+const myName = `opu mia`
+console.log(myName);
--- myJS/string.js
+++ myJS/string.js
@@ -1,14 +1,46 @@
-const name ='Ryhan rohman'
-const studentId = 578010
+const name ='Ryhan'
+const studentId = 578011
 
-// console.log("my name is ${name} my studentId is ${studentId} "); 
+console.log( `my name is ${name} my studentId is ${studentId}`); // ( ` )backtick
 
-const gameName = new String ("opu mia")
+const gameName = new String ("opumia")
 console.log(gameName);
 
 console.log(gameName. length);
+
+console.log(gameName.toLocaleLowerCase);
 console.log(gameName.toUpperCase());
 
+console.log(gameName.charAt(0));
+console.log(gameName.charAt(1));
+console.log(gameName.charAt(2));
+console.log(gameName.charAt(3));
+console.log(gameName.charAt(4));
 console.log(gameName.charAt(5));
 
+
+console.log(gameName.indexOf("o"));
 console.log(gameName.indexOf("p"));
+console.log(gameName.indexOf("u"));
+
+
+const newstring = gameName.substring(0, 5)
+console.log(newstring);
+
+
+const ourDay = "360Day"
+console.log(ourDay);
+
+//const newstring = ourDay .substring(0, 4)
+
+const myEmail = "    www.muname4585@gmail.com    "
+console.log(myEmail);
+console.log(myEmail.trim()); // word space avoiding
+
+const ourData = "        karim  is a good Student but rohim is not a goot student       "
+console.log(ourData);
+console.log(ourData.trim());// word space avoiding
+
+const url ="https//:rohmanahmed.com/rohman%20mia"
+console.log(url.replace("%20","-"))
+
