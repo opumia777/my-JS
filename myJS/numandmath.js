6200af68540ddc0fa82f9c264d6664274a567a86
opu mia <opumia7799@gmail.com>
2024-07-14 03:47:57 +0000

    math

--- myJS/numandmath.js
+++ myJS/numandmath.js
@@ -1,2 +1,11 @@
 const myName = `opu mia`
 console.log(myName);
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(5.33));
+console.log(Math.round(5.50));
+console.log(Math.min(2, 5, 4, 9, 3, 4));
+console.log(Math.max(5, 2, 9, 4, 5, 5));
+
+console.log(Math.random());
