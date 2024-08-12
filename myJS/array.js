2024f19b801cad88421dbaf932320c1690c98f0c
opu mia <opumia7799@gmail.com>
2024-08-12 05:37:20 +0000

    array

---
+++ myJS/array.js
@@ -0,0 +1,40 @@
+//while loop
+
+var sp = 30;
+var ep = 9;
+
+while (sp <= ep) {
+    console.log(sp);
+    sp++;
+    
+}
+
+//do while loop
+
+do {
+    console.log(sp);
+    sp++;
+    
+}while(sp <= ep);
+ 
+//for loop
+
+for (var i = 0; i <= 20; i++) {
+    console.log(i);
+     
+    
+}
+var g =5;
+for (var m = 1; m <= 10; m++) {
+    console.log(g +" x " + m + " =  " + g*m);
+
+    
+}
+
+for (var F = 0; F <= 100; F++) {
+    if ( F > 7 && F % 7 === 6) {
+        console.log(F);
+        
+    }
+    
+}
